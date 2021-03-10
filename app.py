7157223faa45b33423d3311a26b019069a59459b
Pepe García <pepe@pepegar.com>
2021-03-10 20:13:07 +0100

    adding database migrations

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+from operator import lshift
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from wtforms import StringField, PasswordField
 from wtforms.validators import Length, Email
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "THIS IS A SECRET, DON'T DO THIS!"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
 Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    email = db.Column(db.String(128), primary_key=True)
+    password = db.Column(db.String(128))
 
 
 class LoginForm(FlaskForm):
